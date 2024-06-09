63a74e56d655fd5f55bf75289e38bb8e3560fdee
Kishan Bapodra <kishanbapodra2002@gmail.com>
2024-06-10 01:11:25 +0400

    Style

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -3,8 +3,8 @@ import { IoMdClose } from "react-icons/io";
 
 export default function Popup() {
   return (
-    <dialog className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 overflow-y-auto h-full w-full ">
-      <div className="bg-secondary p-6 rounded shadow-lg flex flex-col gap-5">
+    <dialog className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 overflow-y-auto backdrop-blur-sm h-full w-full">
+      <div className="bg-secondary p-6 rounded shadow-orange-500 shadow-lg flex flex-col gap-5">
         <div className="flex justify-between gap-10">
           <h2 className="text-xl flex items-center">Hello, Popup here</h2>
           <Link href="/popup">
